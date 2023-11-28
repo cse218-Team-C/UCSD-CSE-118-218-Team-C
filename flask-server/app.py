74526735cbc5341789b5fe79a771b61459874eec
Varun Singh <v7singh@ucsd.edu>
2023-11-27 18:36:15 -0800

    int channel added

--- flask-server/app.py
+++ flask-server/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-@app.route('/moisture', methods=['GET'])
-def get_moisture():
+@app.route('/moisture/<int:channel>', methods=['GET'])
+def get_moisture(channel):
     # TODO: Replace with actual moisture level retrieval code
     moisture_level = 0
-    return jsonify({'moisture_level': moisture_level})
+    return jsonify({'moisture_level': moisture_level, 'channel': channel})
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
