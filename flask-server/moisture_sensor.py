768bd9394308445ee8f2349d1f236c134e8f055d
Sanjai Subramanian <s8subram@ucsd.edu>
2023-12-07 15:43:37 -0800

    Replaced GPiO with I2C moisture sensor reading

--- flask-server/moisture_sensor.py
+++ flask-server/moisture_sensor.py
@@ -1,18 +1,22 @@
 import RPi.GPIO as GPIO
+import smbus
 
 # gets the moisture reading of a list of sensors, 
 #  connected at the channels identified by in_channels
 # the default value is arbitrary and used only for debugging
 def get_moisture_readings_from_channels (in_channels: list[int] = [17]) -> dict[int, float]:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(in_channels, GPIO.IN)
+
+    address = 0x48
+    A0 = 0x40
+    bus = smbus.SMBus(1)
     
     moisture_readings: dict[int, float] = {}
     
-    for channel in in_channels:
-        moisture_readings[channel] = GPIO.input(channel)
+    bus.write_byte(address, A0)
+    value = bus.read_byte(address)
     
-    GPIO.cleanup()
+    for channel in in_channels:
+        moisture_readings[channel] = value
     
     return moisture_readings
     
